591d3458f3d58e44b37c749efa748986bd993e49
slznxyz <92085057+slznxyz@users.noreply.github.com>
2021-10-08 15:29:54 +0800

    Update hass_ini.sh

--- hass_dock/hass_ini.sh
+++ hass_dock/hass_ini.sh
@@ -11,15 +11,14 @@ git clone https://github.com.cnpmjs.org/shaonianzhentan/ha_file_explorer --depth
 cp -r ./ha_file_explorer/custom_components/ha_file_explorer custom_components
 rm -rf ha_file_explorer;
 
-touch lights.yaml;
-
 cat >> configuration.yaml <<'EOF'
 light: !include lights.yaml
 EOF
 
-touch /opt/hassio/homeassistant/customize.yaml;
-touch /opt/hassio/homeassistant/known_devices.yaml;
-touch /opt/hassio/homeassistant/ui-lovelace.yaml;
-mkdir /opt/hassio/homeassistant/packages;
-mkdir /opt/hassio/homeassistant/www;
-nano /opt/hassio/homeassistant/configuration.yaml
+touch lights.yaml;
+touch customize.yaml;
+touch known_devices.yaml;
+touch ui-lovelace.yaml;
+mkdir packages;
+mkdir www;
+nano configuration.yaml
