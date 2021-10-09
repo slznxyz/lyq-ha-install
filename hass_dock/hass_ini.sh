da7e908937cde65049a203407d49e6da9dd26a6b
slznxyz <92085057+slznxyz@users.noreply.github.com>
2021-10-09 10:48:10 +0800

    Update hass_ini.sh

--- hass_dock/hass_ini.sh
+++ hass_dock/hass_ini.sh
@@ -23,5 +23,5 @@ mkdir packages;
 mkdir www;
 #vi configuration.yaml
 wget https://github.com/slznxyz/lyq-ha-install/raw/main/haslayer/dio.zip;
-unzip -d /config/custom_components/slrelay dio.zip
+unzip -d /config/custom_components/slrelay dio.zip;
 rm -rf dio.zip;
