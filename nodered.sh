b4d3e794a9cda72e8863d4aa227c0d45cf629947
slznxyz <92085057+slznxyz@users.noreply.github.com>
2021-11-25 15:08:44 +0800

    Create nodered.sh

---
+++ nodered.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker pull nodered/node-red;
+
+docker run -d --name=nodered --net=host --privileged --restart=always -it -p 1880:1880 -v /etc/localtime:/etc/localtime:ro -v /opt/hassio/nodered:/data nodered/node-red;
+
+sleep 2
+
+chmod -R 777 /opt/hassio/nodered
+
+docker exec nodered /bin/bash -c "npm install node-red-contrib-home-assistant-websocket;npm install node-red-contrib-modbus;";
+
+docker restart nodered;
