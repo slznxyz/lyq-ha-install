356450c7df32b1e7f9de43e13c7027ba9bca7b60
slznxyz <92085057+slznxyz@users.noreply.github.com>
2021-12-03 11:54:42 +0800

    Update utils.js

--- ha-config/www/amap/libs/utils.js
+++ ha-config/www/amap/libs/utils.js
@@ -1,395 +1,224 @@
-import "./libs/style-element.js";
-import "https://webapi.amap.com/loader.js";
-import {
-    wgs84togcj02,
-    getEntityName,
-    getEntityShortName,
-} from "./libs/utils.js?module";
+//定义一些常量
+var x_PI = (3.14159265358979324 * 3000.0) / 180.0;
+var PI = 3.1415926535897932384626;
+var offset_a = 6378245.0;
+var offset_ee = 0.00669342162296594323;
 
-const { Element, html } = Polymer;
+/**
+ * 百度坐标系 (BD-09) 与 火星坐标系 (GCJ-02)的转换
+ * 即 百度 转 谷歌、高德
+ * @param bd_lon
+ * @param bd_lat
+ * @returns {*[]}
+ */
+export function bd09togcj02(bd_lon, bd_lat) {
+    var x_pi = (3.14159265358979324 * 3000.0) / 180.0;
+    var x = bd_lon - 0.0065;
+    var y = bd_lat - 0.006;
+    var z = Math.sqrt(x * x + y * y) - 0.00002 * Math.sin(y * x_pi);
+    var theta = Math.atan2(y, x) - 0.000003 * Math.cos(x * x_pi);
+    var gg_lng = z * Math.cos(theta);
+    var gg_lat = z * Math.sin(theta);
+    return [gg_lng, gg_lat];
+}
 
-class AmapPanel extends Element {
-    static get properties() {
-        return {
-            hass: { type: Object, observer: "drawEntitiesDebounced" },
-            narrow: { type: Boolean },
-            route: { type: Object },
-            panel: { type: Object },
-        };
-    }
+/**
+ * 火星坐标系 (GCJ-02) 与百度坐标系 (BD-09) 的转换
+ * 即谷歌、高德 转 百度
+ * @param lng
+ * @param lat
+ * @returns {*[]}
+ */
+export function gcj02tobd09(lng, lat) {
+    var z = Math.sqrt(lng * lng + lat * lat) + 0.00002 * Math.sin(lat * x_PI);
+    var theta = Math.atan2(lat, lng) + 0.000003 * Math.cos(lng * x_PI);
+    var bd_lng = z * Math.cos(theta) + 0.0065;
+    var bd_lat = z * Math.sin(theta) + 0.006;
+    return [bd_lng, bd_lat];
+}
 
-    constructor() {
-        super();
-        this.amapMap = null;
-        this.drawEntitiesDebounced = this.debounce(
-            this.drawEntities.bind(this),
-            500
-        );
-        this.fitMapDebounced = this.debounce(this.fitMap, 500);
-        this.zones = [];
-        this.trackers = [];
-    }
-
-    connectedCallback() {
-        super.connectedCallback();
-        this.initAmap();
-    }
-
-    disconnectedCallback() {
-        super.disconnectedCallback();
-        this.amapMap && this.amapMap.destroy();
-    }
-
-    ready() {
-        console.log("ready");
-        super.ready();
-    }
-
-    initAmap() {
-        const { longitude, latitude } = this.hass.config;
-        const { darkMode } = this.hass.themes;
-        const { key } = this.panel.config;
-        AMapLoader.load({
-            key, // 申请好的Web端开发者Key，首次调用 load 时必填
-            plugins: ["AMap.Scale", "AMap.ToolBar"], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
-        })
-            .then((AMap) => {
-                const amapMapId = this.shadowRoot.querySelector("#AmapMap");
-                const amapMapOption = {
-                    resizeEnable: true,
-                    center: wgs84togcj02(longitude, latitude),
-                    zoom: 13,
-                    defaultCursor: "pointer",
-                    mapStyle: darkMode
-                        ? "amap://styles/dark"
-                        : "amap://styles/normal",
-                };
-                const toolBar = new AMap.ToolBar({
-                    liteStyle: true,
-                });
-                const scale = new AMap.Scale();
-
-                this.amapMap = new AMap.Map(amapMapId, amapMapOption);
-                this.amapMap.addControl(toolBar);
-                this.amapMap.addControl(scale);
-                this.amapMap.on("complete", () => {
-                    console.log("amap complete");
-                    this.amapMapTrafficLayer = new AMap.TileLayer.Traffic({
-                        autoRefresh: true,
-                    });
-                    this.amapMapTrafficLayer.hide();
-                    this.amapMapTrafficLayer.setMap(this.amapMap);
-
-                    this.drawZones(this.hass);
-                    this.drawEntitiesDebounced(this.hass);
-                    this.fitMapDebounced();
-                });
-            })
-            .catch((e) => {
-                console.error(e); //加载错误提示
-            });
-    }
-
-    clearMap() {
-        this.amapMap && this.amapMap.clearMap();
-    }
-
-    fitMap() {
-        this.amapMap && this.amapMap.setFitView();
-    }
-
-    async fetchHistoryPath(start_time, end_time, entity_id) {
-        const [[history]] = await this.hass.callApi(
-            "GET",
-            `history/period/${start_time}?end_time=${end_time}&minimal_response${
-                entity_id ? `&filter_entity_id=${entity_id}` : ``
-            }`
-        );
-        let path = [];
-        for (let state of history) {
-            const { latitude, longitude } = state.attributes;
-            const lnglat = wgs84togcj02(longitude, latitude);
-            path.push(lnglat);
-        }
-        return path;
-    }
-
-    toggleTrafffic(e) {
-        if (e.target.checked) {
-            this.amapMapTrafficLayer && this.amapMapTrafficLayer.show();
-        } else {
-            this.amapMapTrafficLayer && this.amapMapTrafficLayer.hide();
-        }
-    }
-
-    toggleTrack(e) {
-        if (e.target.checked) {
-            const originPath = fetchHistoryPath(
-                new Date(new Date().setHours(0, 0, 0)).toISOString(),
-                new Date(new Date().setHours(23, 59, 59)).toISOString(),
-                e.target.data
-            );
-            const polyline = new AMap.Polyline({
-                path: originPath,
-                showDir: true,
-                strokeColor: "#28F", //线颜色
-                strokeWeight: 6, //线宽
-                extData: `track:${e.target.data}`,
-                // strokeOpacity: 1,     //线透明度
-                // strokeStyle: "solid"  //线样式
-            });
-            this.amapMap.add(polyline);
-            this.fitMapDebounced();
-        } else {
-            const polylines = this.amapMap.getAllOverlays("polyline");
-            for (let polyline of polylines) {
-                const extData = polyline.getExtData();
-                if (extData === `track:${e.target.data}`) {
-                    this.amapMap.remove(polyline);
-                }
-            }
-        }
-    }
-
-    drawZones(hass) {
-        if (!this.amapMap || !hass || !hass.states) return;
-        this.zones = Object.keys(hass.states).filter((state) => {
-            const { attributes } = hass.states[state];
-            return (
-                state.indexOf("zone") === 0 &&
-                "longitude" in attributes &&
-                "latitude" in attributes
-            );
-        });
-        for (let zone of this.zones) {
-            this.drawZone(hass.states[zone]);
-        }
-    }
-
-    drawEntities(hass) {
-        if (!this.amapMap || !hass || !hass.states) return;
-        this.trackers = Object.keys(hass.states).filter((state) => {
-            const { attributes } = hass.states[state];
-            return (
-                // attributes.source_type === "gps" &&
-                state.indexOf("device_tracker") === 0 &&
-                "longitude" in attributes &&
-                "latitude" in attributes
-            );
-        });
-        const _trackers = this.trackers.slice();
-
-        const markers = this.amapMap.getAllOverlays("marker");
-        for (let marker of markers) {
-            // entity_id
-            const state = marker.getExtData();
-            // 排除zone
-            if (state.indexOf("zone") === 0) continue;
-            if (!this.trackers.includes(state)) {
-                // 移除点
-                this.amapMap.remove(marker);
-                console.log(`Del:${state}`);
-                continue;
-            }
-            // 更新点
-            const { longitude, latitude } = hass.states[state].attributes;
-            const lnglat = wgs84togcj02(longitude, latitude);
-            marker.setPosition(new AMap.LngLat(lnglat[0], lnglat[1]));
-            console.log(`Mov:${state}`);
-            // 剩余部分
-            if (_trackers.includes(state)) {
-                _trackers.splice(_trackers.indexOf(state), 1);
-            }
-        }
-        // 添加点
-        for (let tracker of _trackers) {
-            this.drawTracker(hass.states[tracker]);
-        }
-    }
-
-    drawZone(state) {
-        if (!this.amapMap || !state) return;
-        const { longitude, latitude, icon, radius, passive } = state.attributes;
-        if (passive) return;
-        const title = getEntityName(state);
-        const lnglat = wgs84togcj02(longitude, latitude);
-        // 添加图标
-        const marker = new AMap.Marker({
-            position: lnglat,
-            offset: new AMap.Pixel(-12, -12),
-            title,
-            extData: state.entity_id,
-            content: `<ha-icon icon=${icon}></ha-icon>`,
-        });
-        marker.on("click", () => {
-            this.fire("hass-more-info", { entityId: state.entity_id });
-        });
-        this.amapMap.add(marker);
-        // 添加圆形区域
-        const circle = new AMap.Circle({
-            center: lnglat,
-            radius: radius,
-            extData: state.entity_id,
-            strokeColor: "#3366FF",
-            strokeOpacity: 0.3,
-            strokeWeight: 3,
-            fillColor: "#FFA500",
-            fillOpacity: 0.35,
-        });
-        this.amapMap.add(circle);
-        console.log(`Add:${state.entity_id}`);
-        this.fitMapDebounced();
-    }
-
-    drawTracker(state) {
-        if (!this.amapMap || !state) return;
-        if (state.state === "home") return;
-        const { attributes } = state;
-        const { longitude, latitude, entity_picture } = attributes;
-        const lnglat = wgs84togcj02(longitude, latitude);
-        const title = getEntityName(state);
-        const short_title = getEntityShortName(state);
-
-        const content = entity_picture
-            ? `<iron-image sizing="cover" class="fit" src="${entity_picture}" ></iron-image>`
-            : `<div class="device-marker"> ${short_title}</div>`;
-        const marker = new AMap.Marker({
-            position: lnglat,
-            offset: new AMap.Pixel(-20, -20),
-            extData: state.entity_id,
-            title,
-            content: content,
-        });
-        marker.on("click", () => {
-            this.fire("hass-more-info", { entityId: state.entity_id });
-        });
-        this.amapMap.add(marker);
-        console.log(`Add:${state.entity_id}`);
-        this.fitMapDebounced();
-    }
-
-    debounce(func, wait, immediate) {
-        let timeout;
-        return function (...args) {
-            const context = this;
-            const later = () => {
-                timeout = null;
-                if (!immediate) func.apply(context, args);
-            };
-            const callNow = immediate && !timeout;
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-            if (callNow) func.apply(context, args);
-        };
-    }
-
-    fire(type, data) {
-        const event = new Event(type, {
-            bubbles: true,
-            cancelable: false,
-            composed: true,
-        });
-        event.detail = data;
-        this.dispatchEvent(event);
-    }
-
-    // _attachDom(dom) {
-    //     this.appendChild(dom);
-    // }
-
-    static get template() {
-        return html`
-            <meta
-                name="viewport"
-                content="initial-scale=1.0, user-scalable=no, width=device-width"
-            />
-            <style include="ha-style style-element">
-                #AmapMap {
-                    position: relative;
-                    width: 100%;
-                    height: calc(100vh - 64px);
-                    overflow: hidden;
-                    z-index: 0;
-                }
-                .input-card {
-                    display: flex;
-                    flex-direction: column;
-                    word-wrap: break-word;
-                    background-color: var(--primary-background-color);
-                    background-clip: border-box;
-                    color: var(--primary-text-color);
-                    border-radius: 5px;
-                    border-width: 0;
-                    box-shadow: 0 2px 6px 0 var(--primary-background-color);
-                    position: fixed;
-                    flex: 1 1 auto;
-                    padding: 10px;
-                    width: auto;
-                    top: 74px;
-                    right: 10px;
-                    bottom: auto;
-                }
-                .input-item {
-                    position: relative;
-                    display: flex;
-                    flex-wrap: wrap;
-                    align-items: center;
-                    width: 100%;
-                    height: 24px;
-                }
-                .device-marker {
-                    position: relative;
-                    display: block;
-                    margin: 0 auto;
-                    width: 38px;
-                    text-align: center;
-                    height: 38px;
-                    line-height: 38px;
-                    font-size: 14px;
-                    border-radius: 50%;
-                    border: 1px solid
-                        var(--ha-marker-color, var(--default-primary-color));
-                    color: var(--primary-text-color);
-                    background-color: var(--primary-background-color);
-                }
-                iron-image {
-                    position: relative;
-                    display: block;
-                    margin: 0 auto;
-                    width: 38px;
-                    text-align: center;
-                    height: 38px;
-                    border: 1px solid
-                        var(--ha-marker-color, var(--default-primary-color));
-                    border-radius: 50%;
-                }
-                ha-icon {
-                    color: var(--primary-text-color);
-                    width: 24px;
-                    height: 24px;
-                }
-            </style>
-            <ha-app-layout>
-                <app-header fixed slot="header">
-                    <app-toolbar>
-                        <ha-menu-button
-                            hass="[[hass]]"
-                            narrow="[[narrow]]"
-                        ></ha-menu-button>
-                        <div main-title>[[panel.title]]</div>
-                    </app-toolbar>
-                </app-header>
-                <div id="AmapMap"></div>
-                <div class="input-card">
-                    <div class="input-item">
-                        <label>
-                            <input type="checkbox" on-click="toggleTrafffic" />
-                            实时路况
-                        </label>
-                    </div>
-                </div>
-            </ha-app-layout>
-        `;
+/**
+ * WGS84转GCj02
+ * @param lng
+ * @param lat
+ * @returns {*[]}
+ */
+export function wgs84togcj02(lng, lat) {
+    if (out_of_china(lng, lat)) {
+        return [lng, lat];
+    } else {
+        var dlat = transformlat(lng - 105.0, lat - 35.0);
+        var dlng = transformlng(lng - 105.0, lat - 35.0);
+        var radlat = (lat / 180.0) * PI;
+        var magic = Math.sin(radlat);
+        magic = 1 - offset_ee * magic * magic;
+        var sqrtmagic = Math.sqrt(magic);
+        dlat =
+            (dlat * 180.0) /
+            (((offset_a * (1 - offset_ee)) / (magic * sqrtmagic)) * PI);
+        dlng =
+            (dlng * 180.0) / ((offset_a / sqrtmagic) * Math.cos(radlat) * PI);
+        var mglat = lat + dlat;
+        var mglng = lng + dlng;
+        return [mglng, mglat];
     }
 }
-customElements.define("amap-panel", AmapPanel);
+
+/**
+ * GCJ02 转换为 WGS84
+ * @param lng
+ * @param lat
+ * @returns {*[]}
+ */
+export function gcj02towgs84(lng, lat) {
+    if (out_of_china(lng, lat)) {
+        return [lng, lat];
+    } else {
+        var dlat = transformlat(lng - 105.0, lat - 35.0);
+        var dlng = transformlng(lng - 105.0, lat - 35.0);
+        var radlat = (lat / 180.0) * PI;
+        var magic = Math.sin(radlat);
+        magic = 1 - offset_ee * magic * magic;
+        var sqrtmagic = Math.sqrt(magic);
+        dlat =
+            (dlat * 180.0) /
+            (((offset_a * (1 - offset_ee)) / (magic * sqrtmagic)) * PI);
+        dlng =
+            (dlng * 180.0) / ((offset_a / sqrtmagic) * Math.cos(radlat) * PI);
+        mglat = lat + dlat;
+        mglng = lng + dlng;
+        return [lng * 2 - mglng, lat * 2 - mglat];
+    }
+}
+
+export function transformlat(lng, lat) {
+    var ret =
+        -100.0 +
+        2.0 * lng +
+        3.0 * lat +
+        0.2 * lat * lat +
+        0.1 * lng * lat +
+        0.2 * Math.sqrt(Math.abs(lng));
+    ret +=
+        ((20.0 * Math.sin(6.0 * lng * PI) + 20.0 * Math.sin(2.0 * lng * PI)) *
+            2.0) /
+        3.0;
+    ret +=
+        ((20.0 * Math.sin(lat * PI) + 40.0 * Math.sin((lat / 3.0) * PI)) *
+            2.0) /
+        3.0;
+    ret +=
+        ((160.0 * Math.sin((lat / 12.0) * PI) +
+            320 * Math.sin((lat * PI) / 30.0)) *
+            2.0) /
+        3.0;
+    return ret;
+}
+
+export function transformlng(lng, lat) {
+    var ret =
+        300.0 +
+        lng +
+        2.0 * lat +
+        0.1 * lng * lng +
+        0.1 * lng * lat +
+        0.1 * Math.sqrt(Math.abs(lng));
+    ret +=
+        ((20.0 * Math.sin(6.0 * lng * PI) + 20.0 * Math.sin(2.0 * lng * PI)) *
+            2.0) /
+        3.0;
+    ret +=
+        ((20.0 * Math.sin(lng * PI) + 40.0 * Math.sin((lng / 3.0) * PI)) *
+            2.0) /
+        3.0;
+    ret +=
+        ((150.0 * Math.sin((lng / 12.0) * PI) +
+            300.0 * Math.sin((lng / 30.0) * PI)) *
+            2.0) /
+        3.0;
+    return ret;
+}
+
+/**
+ * 判断是否在国内，不在国内则不做偏移
+ * @param lng
+ * @param lat
+ * @returns {boolean}
+ */
+export function out_of_china(lng, lat) {
+    return (
+        lng < 72.004 || lng > 137.8347 || lat < 0.8293 || lat > 55.8271 || false
+    );
+}
+
+/**
+ * ISO时间转成时间戳
+ */
+export function getTimeIsoToTs(ms) {
+    // 把时间的中的T和Z 替换成空字符串
+    let date = ms.replace("T", " ");
+    let data = date.replace("Z", "");
+    // 声明一个变量赋值给：日期时间字符串，并返回 1970/1/1 午夜距离该日期时间的毫秒数
+    let datime = Date.parse(data);
+    let time = new Date();
+    let msi = time.getTime();
+    return msi;
+}
+
+/**
+ * 时间戳转换ISO
+ * @param {*} tm
+ */
+export function getTimeTsToIso(tm) {
+    let _tm = tm;
+    let date = new Date(tm);
+    date.getTime(_tm * 1000);
+    // console.log(date.toDateString());//Mon Mar 11 2019
+    // console.log(date.toGMTString()); //Mon, 11 Mar 2019 06:55:07 GMT
+    // console.log(date.toISOString()); //2019-03-11T06:55:07.622Z
+    return date.toISOString();
+}
+
+/**
+ * 获取实体类型
+ */
+export function getEntityDomain(entity) {
+    if (!entity) return "";
+    const { entity_id } = entity;
+    return entity_id.substr(0, entity_id.indexOf("."));
+}
+
+/**
+ * 获取实体对象Id
+ * @param {*} entity
+ */
+export function getEntityObjectId(entity) {
+    if (!entity) return "";
+    const { entity_id } = entity;
+    return entity_id.substr(entity_id.indexOf(".") + 1);
+}
+
+/**
+ * 获取实体别名
+ */
+export function getEntityName(entity) {
+    if (!entity) return "";
+    const friendly_name = entity.attributes.friendly_name || "";
+    const object_name = getEntityObjectId(entity).replace(/_/g, " ");
+    return friendly_name || object_name;
+}
+
+/**
+ * 获取实体别名简化
+ * @param {*} entity
+ */
+export function getEntityShortName(entity) {
+    if (!entity) return "";
+    const entity_name = getEntityName(entity);
+    const short_name = entity_name
+        .split(" ")
+        .map((part) => part[0])
+        .join("")
+        .substr(0, 3);
+    return short_name;
+}
