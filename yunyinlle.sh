c1fa719b7743ad2582f110a1b10f3f8fcaf741e4
slznxyz <92085057+slznxyz@users.noreply.github.com>
2021-12-06 16:07:55 +0800

    Update yunyinlle.sh

--- yunyinlle.sh
+++ yunyinlle.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+opkg update;
+opkg install git-http;
+opkg install ca-bundle;
 docker exec nodered /bin/bash -c "npm install md5;npm install music-metadata;npm install qrcode;npm install safe-decode-uri-component;npm install express-fileupload;npm install pac-proxy-agent;npm install tunnel;";
-
 docker restart nodered;
