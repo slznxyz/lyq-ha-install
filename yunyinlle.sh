479c75c3e5308f59d2fa6be10406520bfcff5be9
slznxyz <92085057+slznxyz@users.noreply.github.com>
2021-12-07 18:22:47 +0800

    Update yunyinlle.sh

--- yunyinlle.sh
+++ yunyinlle.sh
@@ -8,5 +8,6 @@ chmod -R 777 /opt/hassio/nodered/NeteaseCloudMusicApi
 cd NeteaseCloudMusicApi;
 cp app.js app.js.bak;
 sed -i 's/3000/3500/' app.js;
-docker exec nodered /bin/bash -c "npm install md5;npm install music-metadata;npm install qrcode;npm install safe-decode-uri-component;npm install express-fileupload;npm install pac-proxy-agent;npm install tunnel;";
+#docker exec nodered /bin/bash -c "npm install md5;npm install music-metadata;npm install qrcode;npm install safe-decode-uri-component;npm install express-fileupload;npm install pac-proxy-agent;npm install tunnel;";
+docker exec nodered /bin/bash -c "cd /data/NeteaseCloudMusicApi && npm install";
 docker restart nodered;
